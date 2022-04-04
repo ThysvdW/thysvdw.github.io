52401ec5cd71e4a468ab03592735155ccd8f87cf
thysvdw <thysvdw@gmail.com>
2022-04-04 18:07:52 +0200

    post 2022-04-04

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220401.1627';
+const cacheName = 'chirpy-20220404.1803';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
