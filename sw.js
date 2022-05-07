afda27885839d57855dda726400f29238490f390
ThysvdW <thysvdw@gmail.com>
2022-05-07 14:47:20 +0200

    commit changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220417.1843';
+const cacheName = 'chirpy-20220507.1445';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
