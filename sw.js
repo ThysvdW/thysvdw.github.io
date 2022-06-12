1e4dbe5d00ada9eea0278f6e24b152a0e1b67187
ThysvdW <thysvdw@gmail.com>
2022-06-12 14:42:35 +0200

    commit changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220612.1437';
+const cacheName = 'chirpy-20220612.1441';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
