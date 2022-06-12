5057ff051b3c118931622d73cdba78b1adb4c356
ThysvdW <thysvdw@gmail.com>
2022-06-12 14:39:05 +0200

    latest update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220507.1445';
+const cacheName = 'chirpy-20220612.1437';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
