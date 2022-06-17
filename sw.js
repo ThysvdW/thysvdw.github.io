9624216553fc7ec7ca80879c6b7cc9d2452ac4de
ThysvdW <thysvdw@gmail.com>
2022-06-17 17:56:05 +0200

    update latest post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220612.1458';
+const cacheName = 'chirpy-20220617.1753';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
