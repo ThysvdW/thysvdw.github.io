c5cdaddee9c7471bf4e8007388c17e54d5dc7c2a
ThysvdW <thysvdw@gmail.com>
2022-07-23 12:23:08 +0200

    fix on sparkline page

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220617.1753';
+const cacheName = 'chirpy-20220723.1220';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
