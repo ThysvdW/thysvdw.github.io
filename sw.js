6e40110a2c4b4d92d159063711076753384fcc9b
Thys <thysvdw@gmail.com>
2022-11-19 11:06:12 +0200

    update page changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221013.2321';
+const cacheName = 'chirpy-20221119.1101';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
